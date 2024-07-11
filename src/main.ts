da126bc25a48d069a71746d56df04373664957e7
raspy <raulpifo@gmail.com>
2024-07-11 18:12:46 +0200

    Added PrimeVue dependency for UI components
    - Added PrimeVue dependency to package.json.
    - Integrated PrimeVue setup into the project.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,26 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: "p",
+      darkModeSelector: "system",
+      cssLayer: false,
+    },
+  },
+});
+//TODO: se pueden importar solo los componentes
+// que utilizas
+//import Button from "primevue/button"
+//app.component('Button', Button);
 app.mount("#app");
